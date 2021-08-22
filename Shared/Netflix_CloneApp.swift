77b64dadc1bd17a3ba9a6a8f098cfa51c01e846c
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-08-22 21:23:43 +0100

    Initial Commit

---
+++ Shared/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Shared
+//
+//  Created by MAC on 22/8/2021.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
